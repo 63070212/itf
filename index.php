dfe096d75ecb24c10a186738282a0123b0a3614f
63070212 <73645482+63070212@users.noreply.github.com>
2020-10-30 16:31:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <h2>Pear</h2>
         <img src="Kazama.jpg">
         <img src="Chikage.jpg">
-        <a href="https://www.dek-d.com/"></a>
+        <a href="https://www.dek-d.com/">dekd</a>
         <p> A: Where does a one-armed man shop?<br>
             B: At a secondhand store.<br>
         </p>
