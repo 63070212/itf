a0d61f161c76af0a179371e5c99a114e928ca9c3
63070212 <73645482+63070212@users.noreply.github.com>
2020-11-19 13:56:22 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,7 +2,7 @@
 
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'max13.mysql.database.azure.com', 'Pear@max13', 'Satin2545', 'ITFLab', 3306);
-if (mysqli_connect_errno($conn)
+if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());')
 }
