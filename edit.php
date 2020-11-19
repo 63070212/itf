f345689115a9cd89c2b118eab37622a732bff9ae
63070212 <73645482+63070212@users.noreply.github.com>
2020-11-19 14:37:44 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -4,7 +4,7 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, 'max13.mysql.database.azure.com', 'Pear@max13', 'Satin2545', 'ITFLab', 3306);
 if (mysqli_connect_erron($conn))
 {
-    die('Failed to connect to MySQL: '.mysqli_connect_error());')
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 $name = $_POST['name'];
@@ -15,7 +15,7 @@ $sql = "UPDATE guestbook SET Comment='$comment' WHERE Name='$name' ";
 if (mysql_query($conn,$sql)) {
     echo "New record created successfully!";
   } else {
-    echo "Error: ".$sql."<br>".mysql_error($conn)
+    echo "Error: ".$sql."<br>".mysql_error($conn);
   }
 
 mysql_close($conn);
